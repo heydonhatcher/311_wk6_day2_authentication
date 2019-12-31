d642dc803a759d99e28c737b226f4bd85a933ea2
heydonhatcher <heyhatch@gmail.com>
2019-12-31 12:39:39 -0600

    ready for grading

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,23 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const logger = () => {}
+const logger = (req, res, next) => {
+  console.log("Logging route:", req.users, new Date().toISOString());
+  next();
+};
 
-const authenticate = () => {}
+const authenticate = (req, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+};
 
 module.exports = {
   logger,
   authenticate
-}
+};
--- sql/connection.js
+++ sql/connection.js
@@ -1,24 +1,25 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
+require("dotenv").config();
 
 class Connection {
   constructor() {
     if (!this.pool) {
-      console.log('creating mysql connection...')
+      console.log("creating mysql connection...");
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: '35.184.42.200',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
-      })
+        host: "35.232.76.72",
+        user: "root",
+        password: process.env.MYSQL_PASSWORD,
+        database: "admin"
+      });
 
-      return this.pool
+      return this.pool;
     }
 
-    return this.pool
+    return this.pool;
   }
 }
 
-const instance = new Connection()
+const instance = new Connection();
 
 module.exports = instance;
